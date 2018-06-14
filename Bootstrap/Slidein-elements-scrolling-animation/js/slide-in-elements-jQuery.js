f540bdb366fa8fba035b18f0a5b376686e633efe
Purvesh Makode <makodepurvesh@gmail.com>
2018-06-14 13:06:03 +0530

    Added source code

---
+++ Bootstrap/Slidein-elements-scrolling-animation/js/slide-in-elements-jQuery.js
@@ -0,0 +1,12 @@
+//Add this jQuery file along with given CSS//
+
+$(window).scroll(function() {
+  $(".slideanim").each(function(){
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+      $(this).addClass("slide");
+    }
+  });
+});
