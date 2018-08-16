5de55881e38984f0d3488f0c2d1e1c53e7276171
Purvesh Makode <makodepurvesh@gmail.com>
2018-08-16 16:02:13 +0530

    add more stuff

---
+++ nodejs/nodejs-upload-files/upload.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.path;
+      var newpath = 'C:/Users/PURVESH/Documents/GitHub/snippets/nodejs/nodejs-upload-files/' + files.filetoupload.name;
+      //specify the path of your choice up here, where the uploaded file get stored.
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File successfully uploaded and Moved!');
+        res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(8080);
+console.log("server is now running on http://localhost8080");
